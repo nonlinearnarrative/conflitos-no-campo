b01defef11b90a7b0db6948129139fc484690136
David van Gelder de Neufville <_@neufv.website>
2018-09-06 17:43:00 +0200

    Switch to ID based thumbnail URLs

--- main.js
+++ main.js
@@ -2,11 +2,9 @@
  * display thumbnails
  */
 const mainEl = document.getElementById('main')
-
 async function fetchData() {
   let response = await fetch('https://io.neufv.website/projects/conflitos/')
   let index = await response.json()
-  let count = 0
 
   for (let folder in index) {
     const spacerEl = document.createElement('div')
@@ -14,9 +12,9 @@ async function fetchData() {
     mainEl.appendChild(spacerEl)
 
     for (let caseDocument of index[folder].documents) {
-      if(caseDocument.thumbnail) {
+      if (caseDocument.id) {
         const thumbnailEl = document.createElement('img')
-        thumbnailEl.src = caseDocument.thumbnail
+        thumbnailEl.src = 'https://drive.google.com/thumbnail?authuser=0&sz=w320&id=' + caseDocument.id
         thumbnailEl.classList.add('thumbnail')
 
         mainEl.appendChild(thumbnailEl)
