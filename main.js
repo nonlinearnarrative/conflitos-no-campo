aa99f36190e8cd1aa443d5bfd1a4e8e359ea38f0
David van Gelder de Neufville <_@neufv.website>
2018-09-07 12:31:48 +0200

    Add language switching

--- main.js
+++ main.js
@@ -4,12 +4,16 @@
 const app = new Vue({
   el: '#main',
   data: {
+    lang: 'PT',
     index: []
   },
   methods: {
     fetchData: async function () {
       let response = await fetch('https://io.neufv.website/projects/conflitos/')
       this.index = await response.json()
+    },
+    setLanguage: function (lang) {
+      this.lang = lang
     }
   },
   mounted: function () {
