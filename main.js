35370988f3ee5019d6dec525a16d715d3bc33d11
David van Gelder de Neufville <_@neufv.website>
2018-09-07 15:30:14 +0200

    Change scale

--- main.js
+++ main.js
@@ -4,13 +4,28 @@
 const app = new Vue({
   el: '#main',
   data: {
+    search: '',
     lang: 'PT',
-    index: []
+    documentIndex: []
+  },
+  computed: {
+    filteredIndex: function() {
+      if (this.search === '') {
+        return this.documentIndex
+      }
+
+      return Object.keys(this.documentIndex)
+        .filter(key => this.search.toLowerCase().includes(key.toLowerCase()))
+        .reduce((obj, key) => {
+          obj[key] = this.documentIndex[key]
+          return obj
+        }, {})
+    }
   },
   methods: {
     fetchData: async function () {
       let response = await fetch('https://io.neufv.website/projects/conflitos/')
-      this.index = await response.json()
+      this.documentIndex = await response.json()
     },
     setLanguage: function (lang) {
       this.lang = lang
