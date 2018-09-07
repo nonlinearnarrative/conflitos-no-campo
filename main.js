58dc23976e69c5a0a2e60309d984c076554759c6
David van Gelder de Neufville <_@neufv.website>
2018-09-07 11:24:09 +0200

    switch to vue

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
 /*
  * display thumbnails
  */
-const mainEl = document.getElementById('main')
-async function fetchData() {
-  let response = await fetch('https://io.neufv.website/projects/conflitos/')
-  let index = await response.json()
-
-  for (let folder in index) {
-    const spacerEl = document.createElement('div')
-    spacerEl.classList.add('spacer')
-    spacerEl.innerHTML = folder + ' &rarr;'
-    mainEl.appendChild(spacerEl)
-
-    for (let caseDocument of index[folder].documents) {
-      if (caseDocument.id) {
-        const thumbnailAnchorEl = document.createElement('a')
-        thumbnailAnchorEl.href = caseDocument.link
-        thumbnailAnchorEl.target = '_blank'
-
-        const thumbnailEl = document.createElement('img')
-        thumbnailEl.src = 'https://drive.google.com/thumbnail?authuser=0&sz=w320&id=' + caseDocument.id
-        thumbnailEl.classList.add('thumbnail')
-
-        thumbnailAnchorEl.appendChild(thumbnailEl)
-        mainEl.appendChild(thumbnailAnchorEl)
-      }
+const app = new Vue({
+  el: '#main',
+  data: {
+    index: []
+  },
+  methods: {
+    fetchData: async function () {
+      let response = await fetch('https://io.neufv.website/projects/conflitos/')
+      this.index = await response.json()
     }
+  },
+  mounted: function () {
+    this.fetchData()
   }
-}
-
-fetchData()
+})
